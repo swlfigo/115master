510db9531403e6c9d8e9d00815f62b1599f6adb3
sylar <swlfigo@gmail.com>
2026-04-21 18:32:42 +0800

    fix(aria2): disable type-definitions rule for mitt events

--- apps/monkey/src/utils/aria2/events.ts
+++ apps/monkey/src/utils/aria2/events.ts
@@ -1,6 +1,8 @@
 import mitt from 'mitt'
 
-interface Events {
+// mitt 要求泛型参数兼容 Record<EventType, unknown>，interface 因无隐式索引签名无法满足
+// eslint-disable-next-line ts/consistent-type-definitions
+type Events = {
   'aria2:open-settings': void
 }
 
